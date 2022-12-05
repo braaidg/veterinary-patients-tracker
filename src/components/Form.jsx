f89fd7231445ecad1f03646071d69c3661d8b81d
Braian Gomez <braiandenisgomez@gmail.com>
2022-12-04 23:21:19 -0300

    Multiples estados aniadidos

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,13 +1,40 @@
+import { useState, useEffect } from "react";
+
 const Form = () => {
+  const [name, setName] = useState("");
+  const [owner, setOwner] = useState("");
+  const [email, setEmail] = useState("");
+  const [date, setDate] = useState("");
+  const [symptoms, setSymptoms] = useState("");
+  const [error, setError] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if ([name, owner, email, date, symptoms].includes("")) {
+      console.log("There's at least 1 field empty");
+      setError(true);
+      return;
+    }
+    setError(false);
+  };
+
   return (
-    <div className="md:w-1/2 lg:w-2/5">
+    <div className="md:w-1/2 lg:w-2/5 mx-5">
       <h2 className="font-black text-3xl text-center">Patients tracker</h2>
       <p className="text-lg mt-5 text-center mb-10">
         Add and <span className="text-indigo-600 font-bold">manage</span>{" "}
         patients
       </p>
 
-      <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
+      >
+        {error && (
+          <div className="bg-red-800 text-white p-3 text-center uppercase font-bold mb-3 rounded">
+            <p>All fields are needed</p>
+          </div>
+        )}
         <div className="mb-5">
           <label
             className="block text-gray-700 uppercase font-bold"
@@ -20,6 +47,8 @@ const Form = () => {
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
             type="text"
             placeholder="Pet name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
         </div>
         <div className="mb-5">
@@ -34,6 +63,8 @@ const Form = () => {
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
             type="text"
             placeholder="Pet owner"
+            value={owner}
+            onChange={(e) => setOwner(e.target.value)}
           />
         </div>
         <div className="mb-5">
@@ -48,6 +79,8 @@ const Form = () => {
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
             type="email"
             placeholder="Contact email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
         <div className="mb-5">
@@ -61,6 +94,8 @@ const Form = () => {
             id="dischargeDate"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
             type="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
           />
         </div>
         <div className="mb-5">
@@ -74,6 +109,8 @@ const Form = () => {
             id="symptoms"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
             placeholder="Describe the symptoms"
+            value={symptoms}
+            onChange={(e) => setSymptoms(e.target.value)}
           />
         </div>
 
